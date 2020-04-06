2b822830459dcc747ba409beb509335c9138a49c
sulivan-dev <sulivan.alvarado21@gmail.com>
2020-04-06 02:59:36 -0600

    Add some TDDs for Threads and Replies testing in the Forum

---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->create();
+        $this->post($thread->path(). '/replies', $reply->toArray());
+
+    }
+
+    /**
+     * @test
+     */
+    public function an_authenticated_user_may_participate_in_forum()
+    {
+        $this->be($user = factory('App\User')->create());
+
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->create();
+        $this->post($thread->path(). '/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $thread;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+
+    /**
+     * @test
+     */
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_a_single_thread()
+    {
+        $response = $this->get($this->thread->path());
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
+
+    }
+
+}
